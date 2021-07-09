662bdd278ffea1c7399be6507c04646ab4245352
arrizkyhp <arrizkyhasya@gmail.com>
2021-07-09 10:24:52 +0700

    menambahkan note.txt dan types.html .js

---
+++ Belajar Javascript/wesbos/Beginner JS/playground/types.js
@@ -0,0 +1,37 @@
+/* eslint-disable */
+console.log('it works');
+// Strings
+
+/*
+
+
+const nama = 'arrizky';
+const middle = 'hasya';
+const last = 'pratama';
+
+const kalimat = 'jum\'at is"cool"  ';
+
+// backtick
+const song = `ohh
+ya
+
+i like
+
+it`;
+
+// Concatenation
+const hello = 'hello nama saya '+ nama +' senang berkenalan denganmu';
+// Concatenation with backtick
+const hello2 = `hello nama saya ${nama} ${middle} senang berkenalan denganmu`;
+
+*/
+
+// ============================================================================
+// Number
+
+const age = 100;
+
+const apple = 10;
+const bocil = 3;
+const tiapAnakDapat = Math.floor(apple/bocil);
+console.log(`tiap anak dapat ${tiapAnakDapat} apple`);

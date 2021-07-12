524d3b8b5239c9b0badc22f8c1ba39baa8095c62
arrizkyhp <arrizkyhasya@gmail.com>
2021-07-12 12:55:52 +0700

    update DOM JS, note_DOM, css & js

--- Belajar Javascript/Sandhika Galih JS/DOM JS/js/domSelection.js
+++ Belajar Javascript/Sandhika Galih JS/DOM JS/js/domSelection.js
@@ -3,11 +3,11 @@ console.log('it works!!');
 // DOM Selection
 // document.getElementsById() => Element
 
-const judul = document.getElementById('judul');
+// const judul = document.getElementById('judul');
 
-judul.style.color = 'violet';
-judul.style.backgroundColor = 'grey';
-judul.innerHTML = 'ArrizkyHP';
+// judul.style.color = 'violet';
+// judul.style.backgroundColor = 'grey';
+// judul.innerHTML = 'ArrizkyHP';
 
 // ==============================================
 // document.getElementsByTagName() => HTMLCollections
@@ -28,7 +28,39 @@ judul.innerHTML = 'ArrizkyHP';
 
 // ==============================================
 // document.getElementsByClassName() => HTMLCOllections
-const p1 = document.getElementsByClassName('p1')[0];
+// const p1 = document.getElementsByClassName('p1')[0];
 
-p1.style.backgroundColor = 'Blue';
-p1.style.color = 'white';
+// p1.style.backgroundColor = 'Blue';
+// p1.style.color = 'white';
+
+// ==============================================
+// document.querySelector; => element
+
+// const p4 = document.querySelector('#b p');
+
+// p4.style.color = 'blue';
+// p4.style.fontSize = '30px';
+
+// const li2 = document.querySelector('#b li:nth-child(2)');
+
+// li2.style.backgroundColor = 'crimson';
+
+// ==============================================
+// document.querySelectorAll(); => NodeList
+
+// const p = document.querySelectorAll('p');
+
+// for (let i = 0; i < p.length; i++) {
+
+//     p[i].style.backgroundColor = 'salmon';
+
+// }
+
+// ==============================================
+// Node Root
+
+const sectionB = document.getElementById('b');
+const p4 = sectionB.getElementsByTagName('p')[0];
+
+
+

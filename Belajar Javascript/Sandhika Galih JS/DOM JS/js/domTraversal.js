bc044f08eabf1f8a67377481ceaca0a16bf67063
arrizkyhp <arrizkyhasya@gmail.com>
2021-07-15 21:12:40 +0700

    update noteDOm & domTraversal.js

--- Belajar Javascript/Sandhika Galih JS/DOM JS/js/domTraversal.js
+++ Belajar Javascript/Sandhika Galih JS/DOM JS/js/domTraversal.js
@@ -15,7 +15,7 @@
 // });
 
 
-
+/*
 // DOM Traversal
 const closeX = document.querySelectorAll('.close');
 
@@ -25,9 +25,41 @@ closeX.forEach((event) => {
 
         // cek e.target di console
         e.target.parentElement.style.display = 'none';
+        // mencegah aksi default <a></a> dimana elemen itu merefresh page
+        e.preventDefault();
+        e.stopPropagation();
     });
 });
 
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(function(card) {
+    card.addEventListener('click', function(event) {
+        alert('ok');
+    });
+}); */
+
+// ketika children memiliki event dan parent juga memeiliki event, saat kita menjalankan event children, event parent juga akan dijalankan.
+// agar tidak dijalankan secara otomatis  digunakan event. stopPropagation();
+
+
+const container = document.querySelector('.container');
+
+container.addEventListener('click', function(e) {
+    // jika yang kita klik punya class close
+    if(e.target.className == 'close') {
+       e.target.parentElement.style.display = 'none';
+    }
+})
+
+
+
+
+
+
+
+
+/*
 // Coba Method
 const nama = document.querySelector('.nama');
 
@@ -40,6 +72,4 @@ console.log(nama.parentElement.parentElement); // output <div class="container".
 // Mengambil Sibling
 console.log(nama.nextSibling); // #text | karena di html ada Enter antara nama dan telp
 console.log(nama.nextElementSibling); // <span class="telp" | mengabaikan enter
-
-
-
+ */

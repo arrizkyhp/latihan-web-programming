aa584bb2d4c70f58a1980fe475accaba3f9dd606
arrizkyhp <arrizkyhasya@gmail.com>
2021-07-20 17:57:08 +0700

    update note.txt dan object.js

--- Belajar Javascript/wesbos/Beginner JS/playground/DataTypes/object.js
+++ Belajar Javascript/wesbos/Beginner JS/playground/DataTypes/object.js
@@ -1,4 +1,4 @@
-const umur = 200;
+/* const umur = 200;
 
 const arrizky = {
   name: 'Arrizky',
@@ -40,4 +40,48 @@ const Luisa = {
   sayHello(greeting = 'Maw') {
     return `${greeting} ${this.name}`;
   },
+}; */
+
+// ===================== References and Value =====================
+
+const name1 = 'ariz';
+const name2 = 'ariz';
+if (name1 === name2) {
+  console.log('its the same');
+}
+
+const person1 = {
+  first: 'ariz',
+  last: 'hasya',
+};
+
+const person2 = {
+  first: 'ariz',
+  last: 'hasya',
+};
+if (person1 === person2) {
+  console.log('its not same');
+}
+
+// copy of the object not reference
+const person3 = { ...person1 }; // ... spread
+person3.first = 'Hasya'; // this will not the same as person1
+
+// merging object
+const meatInventory = {
+  bacon: 2,
+  sausage: 3,
+  oyster: 10,
+};
+
+const veggieInventory = {
+  cabbage: 7,
+  lettuce: 5,
+  oyster: 20,
+};
+
+// read last , oyster gonna be 20
+const refrigator = {
+  ...meatInventory,
+  ...veggieInventory,
 };

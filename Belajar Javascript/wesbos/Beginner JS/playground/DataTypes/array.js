6e984569a29697cc40cbc91b72123d4d3ca5fdfd
arrizkyhp <arrizkyhasya@gmail.com>
2021-07-21 13:33:20 +0700

    update Array html & js

--- Belajar Javascript/wesbos/Beginner JS/playground/DataTypes/array.js
+++ Belajar Javascript/wesbos/Beginner JS/playground/DataTypes/array.js
@@ -42,12 +42,62 @@
 // const names3 = ['Luna', 'Bima', ...names];
 
 // ======================SLice and SPlice =========================================
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(numbers);
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(numbers);
 // numbers.splice(3, 2);
 // console.log(numbers); // [1, 2, 3, 6, 7, 8, 9, 10]
-console.log(numbers.splice(3, 2));
+// console.log(numbers.splice(3, 2));
 
 // numbers.slice(1, 4);
 // console.log(numbers.slice(1, 4)); // [2,3,4]
 // console.log(numbers); // the original still the same
+
+/* const motor = ['suzuki', 'yamaha', 'kawasaki', 'honda'];
+
+// add new item in between
+const newBikes = [
+  // take before trange
+  ...motor.slice(0, 2),
+  // add new
+  'vespa',
+  //   add the rest
+  ...motor.slice(2),
+];
+
+console.log(newBikes); // ["suzuki", "yamaha", "vespa", "kawasaki", "honda"]
+
+// WANT TO REMOVE KAWASAKI
+
+const newBikes2 = [
+  // takes before kawasaki range
+  ...newBikes.slice(0, 3),
+  //   takes after kawasaki range
+  ...newBikes.slice(4),
+];
+
+console.log(newBikes2); // ["suzuki", "yamaha", "vespa", "honda"]
+ */
+
+//  ============================================ Delete Comments ============================================
+const comments = [
+  { text: 'Recomended Seller', id: 123 },
+  { text: 'Barang bagus seller ramah', id: 233 },
+  { text: '1 hari langsung sampe', id: 143 },
+  { text: 'Barang rusak', id: 623 },
+  { text: 'Barang KW seller lambat', id: 253 },
+];
+
+function deleteComment(id, comments) {
+  // first find the index of the item in the array
+  const commentIndex = comments.findIndex((comment) => comment.id === id);
+  // make a new array without that item in it
+  return [
+    ...comments.slice(0, commentIndex),
+    ...comments.slice(commentIndex + 1),
+  ];
+
+  // return our new array
+}
+console.log(comments);
+console.log('Delete 143');
+console.log(deleteComment(143, comments));

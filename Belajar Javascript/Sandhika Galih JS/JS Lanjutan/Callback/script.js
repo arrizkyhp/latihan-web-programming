6042f7b637fc9b36d35520761e59218d7f31a328
arrizkyhp <arrizkyhasya@gmail.com>
2021-07-30 09:14:31 +0700

    update Documentation.html (callback) | SG

--- Belajar Javascript/Sandhika Galih JS/JS Lanjutan/Callback/script.js
+++ Belajar Javascript/Sandhika Galih JS/JS Lanjutan/Callback/script.js
@@ -1,28 +1,30 @@
 // Callback
 // Callback merupakan mekanisme untuk memanggil kembali kode yang ada di program dari proses Async.
 // Callback biasanya dibuat dalam bentuk function, dan function tersebut akan dieksekusi saat proses Async selesai.
-// Dengan menggunakan Callback, program bisa menerima informasi yang dibutuhkan dari proses yang berjalan seaca Async
+// Dengan menggunakan Callback, program bisa menerima informasi yang dibutuhkan dari proses yang berjalan secara Async
 
 
 // Synchronous Callback
-/* function halo(nama) {
-    alert(`Halo ${nama}`);
-}
+// function halo(nama) {
+//     alert(`Halo ${nama}`);
+// }
 
-function tampilkanPesan (callback) {
-    const nama = prompt('Masukkan nama:');
-    callback(nama);
-}
+// function tampilkanPesan (callback) {
+//     const nama = prompt('Masukkan nama:');
+//     callback(nama);
+//     console.log('test');
+// }
 
-// tanpa kurung, jika ditambah kurung langsung  di eksekusi
-tampilkanPesan(halo);
+// // tanpa kurung, jika ditambah kurung langsung  di eksekusi
+// // tampilkanPesan(halo);
 
-// anonymous function
-tampilkanPesan(nama => alert(`Halo ${nama}`)); */
+// // anonymous function
+// tampilkanPesan(nama => alert(`Halo ${nama}`));
 
 // ========================================================
 // Asynchronous
 
+// yang ini masih synchronous
 /* const mhs = [
     {
         "nama": "Arrizky Hasya Pratama",
@@ -47,7 +49,7 @@ tampilkanPesan(nama => alert(`Halo ${nama}`)); */
     },
 ];
 
-// proses harus menunggu dulu forEach baru console.log selesai di eksekusi
+// proses harus menunggu dulu mhs.forEach baru console.log selesai di eksekusi
 console.log('mulai');
 mhs.forEach(m => {
     for (let i = 0; i < 10000000; i++) {
@@ -87,7 +89,6 @@ getDataMahasiswa('data/mahasiswa.json', result => {
 }, () => {
 
 });
-
 console.log('selesai');
 
 
@@ -97,3 +98,16 @@ console.log('selesai');
 // ketika kita menggunakan jquery perlu diingat bahwa kita memanggil library eksternal, ketika kita memanggil library eksternal ada resource yang digunakan.
 // jadi defaultnya performance nya akan lebih lambat, dibanding kita menggunakan bawaan dari javascript.
 
+//  =======================================================================================
+
+// AJAX ??
+// Ajax singkatan dari Asynchronous JavaScript and XML
+// Ajax dapat digunakan untuk mengambil data dari server setelah halaman web tampil
+// AJAX dapat digunakan utnuk mengubah tampilan web tanpa harus me-load ulang web.
+// dengan menggunakan AJAX kita akan memisahkan antara beban kerja backend & frontend, frontend cukup menampilkan website dulu nanti secara lazy akan memanggil backend data menggunakan AJAX
+//  Jadi setelah web tampil maka akan call backend, dengan begitu web terlihat sangat cepat, karena tidak harus menunggu respond backend.
+// AJAX biasanya digunakan untuk mengirim data ke server atau menerima data dari server.
+// Kita tidak bisa langsung mengambil response AJAX, karena proses AJAX adlaah Async, sehingga kita perlu menunggu sampai proses Async nya selesai.
+// untuk mendapatkan informasi AJAX, kita bisa menggunakan AJAX callback, yang akan dieksekusi setelah proses AJAX selesai.
+
+// Ajax dapat mengirim data ke server secara async di backgroud.

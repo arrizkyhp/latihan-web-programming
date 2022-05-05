7902c7ca356f9c1d1f2059f985c996d29f44100a
arrizkyhp <52059831+arrizkyhp@users.noreply.github.com>
2022-05-05 21:08:48 +0700

    update Belajar MERN (ph-mern 39 | CRUD di nodeJS/backend)

--- Belajar Mern/ph-mern/mern-api/src/controllers/blog.js
+++ Belajar Mern/ph-mern/mern-api/src/controllers/blog.js
@@ -1,6 +1,11 @@
 const { validationResult } = require('express-validator')
 const BlogPost = require('../models/blog')
+// untuk mengetahui path image
+const path = require('path')
+// untuk menghapus gambar
+const fs = require('fs')
 
+// Create Blog Post
 exports.createBlogPost = (req, res, next) => {
     const errors = validationResult(req);
 
@@ -48,4 +53,145 @@ exports.createBlogPost = (req, res, next) => {
     });
 
 
+}
+
+// Get All Article Blog Post
+exports.getAllBlogPost = (req, res, next) => {
+    // Memanggil data dari database
+    BlogPost.find()
+    .then(result => {
+        res.status(200).json({
+            message: 'Data Blog Post Berhasil dipanggil',
+            data: result
+        })
+    })
+    .catch(err => {
+        // mengirim error ke index.js
+        next(err);
+    })
+}
+
+// Get Article Blog Post By Id
+exports.getBlogPostById = (req, res, next) => {
+    // Memanggil data dari database
+    const postId = req.params.postId
+    BlogPost.findById(postId)
+    .then(result => {
+        // jika id tidak ditemukan
+        if(!result) {
+            const error = new Error('Blog Post tidak ditemukan')
+            error.errorStatus = 404;
+            throw error;
+        }
+
+        // jika id ditemukan
+        res.status(200).json({
+            message: 'Data Blog Post Berhasil dipanggil',
+            data: result
+        })
+
+    })
+    .catch(err => {
+        // mengirim error ke index.js
+        next(err);
+    })
+}
+
+// Update Article Blog Post (PUT)
+exports.updateBlogPost = (req, res, next) => {
+    const errors = validationResult(req);
+
+    // Validasi data
+    if(!errors.isEmpty()) {
+        console.log(errors)
+        const err = new Error('Input value tidak valid');
+        err.errorStatus = 400;
+        err.data = errors.array();
+        throw err;
+    }
+
+    // validasi jika tidak ada image
+    if(!req.file) {
+        const err = new Error('Image harus di upload');
+        err.errorStatus = 422;
+        throw err;
+    }
+
+    const title = req.body.title;
+    const image = req.file.path;
+    const body = req.body.body;
+    // get id di url
+    const postId = req.params.postId;
+
+    BlogPost.findById(postId)
+    .then( post => {
+        // jika id tidak ditemukan
+        if(!post) {
+            const err = new Error('log Post tidak ditemukan')
+            err.errorStatus = 404;
+            throw err;
+        }   
+
+        // jika id ditemukan
+        post.title = title;
+        post.body = body;
+        post.image = image;
+
+        return post.save();
+
+    })
+    .then(result => {
+        res.status(200).json({
+            message: 'Update berhasil',
+            data: result
+        })
+    })
+    .catch( err => {
+        // mengirim error ke index.js
+        next(err);
+    })
+    
+}
+
+// Delete Article Blog Post
+exports.deleteBlogPost = (req, res, next) => {
+    // get id dari param/url
+    const postId = req.params.postId
+
+    BlogPost.findById(postId)
+    .then(post => {
+         // jika id tidak ditemukan
+        if(!post) {
+            const err = new Error('log Post tidak ditemukan')
+            err.errorStatus = 404;
+            throw err;
+        } 
+        
+        // jika id ditemukan
+        // hapus image
+        removeImage(post.image);
+        // Hapus Postingan di DB
+        return BlogPost.findByIdAndRemove(postId)
+    })
+    .then(result => {
+        res.status(200).json({
+            message: 'Hapus Artikel Berhasil',
+            data: result
+        }) 
+    })
+    .catch(err => {
+        next(err);
+    })
+}
+
+const removeImage = (filePath) => {
+    // dirname: D:\Arrizky\Latihan\latihan-web-programming\Belajar Mern\ph-mern\mern-api\src\controllers
+    // ../.. (keluar 2x)
+    // filePath: images\1651758214975-Ulaanbaatar, Mongolia.jpg
+
+    // HASIL: D:\Arrizky\Latihan\latihan-web-programming\Belajar Mern\ph-mern\mern-api\images\1651758214975-Ulaanbaatar, Mongolia.jpg
+    filePath = path.join(__dirname, '../..', filePath)
+
+    // hapus gambar
+    fs.unlink(filePath, err => console.log(err));
 }
--- Belajar Mern/ph-mern/mern-api/src/routes/blog.js
+++ Belajar Mern/ph-mern/mern-api/src/routes/blog.js
@@ -5,10 +5,25 @@ const router = express.Router();
 
 const blogController = require('../controllers/blog')
 
-// POST : /v1/blog/post
+// CREATE ARTIKEL : /v1/blog/post
 router.post('/post', [
     body('title').isLength({ min: 5 }).withMessage('input Judul minimal 5 karakter'), 
     body('body').isLength({ min: 5 }).withMessage('input Artikel minimal 5 karakter')], 
     blogController.createBlogPost)
+
+// READ ALL ARTIKEL
+router.get('/posts', blogController.getAllBlogPost);
+
+// READ ARTIKEL BY ID
+router.get('/post/:postId', blogController.getBlogPostById)
+
+// UPDATE ARTIKEL (PUT)
+router.put('/post/:postId',[
+    body('title').isLength({ min: 5 }).withMessage('input Judul minimal 5 karakter'), 
+    body('body').isLength({ min: 5 }).withMessage('input Artikel minimal 5 karakter')],
+     blogController.updateBlogPost)
+
+// DELETE ARTIKEL
+router.delete('/post/:postId', blogController.deleteBlogPost)
 
 module.exports = router;

958fcb37658d5327f28a11bdf0e026183e50dd1a
arrizkyhp <52059831+arrizkyhp@users.noreply.github.com>
2022-05-05 20:00:36 +0700

    update Belajar Mern (ph-mern 35 | Upload Image di NodeJS)

--- Belajar Mern/ph-mern/mern-api/index.js
+++ Belajar Mern/ph-mern/mern-api/index.js
@@ -2,14 +2,45 @@ const express = require('express');
 const bodyParser = require('body-parser')
 // mongoose connect mongodb ke node
 const mongoose = require('mongoose')
+// multer untuk upload file/image
+const multer = require('multer');
+// agar gambar bisa diakses
+const path = require('path');
 
 const app = express();
 const authRoutes = require('./src/routes/auth')
 const blogRoutes = require('./src/routes/blog')
 // const productRoutes = require('./src/routes/products');
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        // Folder Images
+        callback(null, 'images');
+    },
+    filename: (req, file, callback) => {
+        // nama file
+        callback(null, new Date().getTime() + '-' + file.originalname)
+    }
+})
+
+const fileFilter = (req, file, callback) => {
+    if(
+        file.mimetype === 'image/png' || 
+        file.mimetype === 'image/jpg' || 
+        file.mimetype === 'image/jpeg'
+    ) {
+        callback(null, true)
+    } else {
+        callback(null, false)
+    }
+}
+
 // Body Parser
 app.use(bodyParser.json()) // type JSON
+// middleware agar gambar bisa diakses
+app.use('/images', express.static(path.join(__dirname, 'images')))
+// middleware Upload gambar
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'))
 
 // CORS
 app.use((req, res, next) => {
--- Belajar Mern/ph-mern/mern-api/src/controllers/blog.js
+++ Belajar Mern/ph-mern/mern-api/src/controllers/blog.js
@@ -2,24 +2,32 @@ const { validationResult } = require('express-validator')
 const BlogPost = require('../models/blog')
 
 exports.createBlogPost = (req, res, next) => {
-    const title = req.body.title;
-    // const image = req.body.image;
-    const body = req.body.body;
-
     const errors = validationResult(req);
 
+    // Validasi data
     if(!errors.isEmpty()) {
         console.log(errors)
-        const err = new Error('Input Value tidak valid');
+        const err = new Error('Input value tidak valid');
         err.errorStatus = 400;
         err.data = errors.array();
         throw err;
-
     }
+
+    // validasi jika tidak ada image
+    if(!req.file) {
+        const err = new Error('Image harus di upload');
+        err.errorStatus = 422;
+        throw err;
+    }
+
+    const title = req.body.title;
+    const image = req.file.path;
+    const body = req.body.body;
 
     const Posting = new BlogPost({
         title: title,
         body: body,
+        image: image,
         author: {
             uid: '1',
             name: 'arrizkyhp'

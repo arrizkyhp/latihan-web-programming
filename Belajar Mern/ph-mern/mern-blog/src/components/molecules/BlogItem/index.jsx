dbed23d19cd671df4333ccd0bf511b6f45b77588
arrizkyhp <52059831+arrizkyhp@users.noreply.github.com>
2022-05-06 10:58:46 +0700

    update Belajar Mern (ph-mern 43 | Install Axios & consume API)

--- Belajar Mern/ph-mern/mern-blog/src/components/molecules/BlogItem/index.jsx
+++ Belajar Mern/ph-mern/mern-blog/src/components/molecules/BlogItem/index.jsx
@@ -3,14 +3,16 @@ import { RegisterBg } from '../../../assets'
 import { Button } from '../../atoms'
 import './blogItem.scss'
 
-const BlogItem = () => {
+const BlogItem = (props) => {
+  // destructuring
+  const {image, title, name, date, body} = props;
   return (
     <div className='blog-item'>
-        <img className='image-thumb' src={RegisterBg} alt="post" />
+        <img className='image-thumb' src={image} alt="post" />
         <div className="content-detail">
-            <p className='title'>Title Blog</p>
-            <p className='author'>Author - Date Post</p>
-            <p className='body'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni consectetur distinctio adipisci a dicta dolore, dolor neque eveniet qui enim ratione architecto totam voluptatum at perspiciatis deserunt cumque. Porro accusamus odit voluptas sed nobis, blanditiis cupiditate ratione. Consectetur, possimus impedit consequuntur sit debitis ipsa est corporis consequatur architecto corrupti optio pariatur officia deleniti mollitia numquam tempora. Earum, inventore quidem cum ea voluptate animi porro sit assumenda minus obcaecati repellendus a perferendis suscipit fuga enim tempora possimus tempore molestias iste laudantium! Assumenda culpa veniam amet porro sapiente consequuntur iure, maxime pariatur cum voluptatibus in rem architecto quia vero ratione aut ipsam.</p>
+            <p className='title'>{title}</p>
+            <p className='author'>{name} - {date}</p>
+            <p className='body'>{body}</p>
             <Button title="View Detail" linkUrl to="/detail-blog" />
         </div>
     </div>
--- Belajar Mern/ph-mern/mern-blog/src/pages/Home/index.jsx
+++ Belajar Mern/ph-mern/mern-blog/src/pages/Home/index.jsx
@@ -1,20 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BlogItem, Button } from '../../components'
 import './home.scss'
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import dateFormat from 'dateformat';
 
 const Home = () => {
+  const [dataBlog, setDataBlog] = useState([]);
   const navigate = useNavigate();
+
+  useEffect (() => {
+    axios.get('http://localhost:4000/v1/blog/posts?page=1&perPage=4')
+    .then(result => {
+      const responseAPI = result.data
+      
+      setDataBlog(responseAPI.data)
+      console.log(responseAPI.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
+
   return (
     <div className='home-page-wrapper'>
       <div className="create-wrapper">
         <Button title="create blog" onClick={() => navigate('/create-blog')}/>
       </div>
       <div className="content-home-wrapper">
-        <BlogItem />
-        <BlogItem />
-        <BlogItem />
-        <BlogItem />
+        {dataBlog.map(blog => {
+          return ( 
+          <BlogItem 
+            key={blog._id}
+            image={`http://localhost:4000/${blog.image}`}
+            title={blog.title}
+            name={blog.author.name}
+            date={dateFormat(blog.createdAt, "dd, mmmm, yyyy")}
+            body={blog.body}
+          /> 
+          )
+        })}
+
       </div>
       <div className="pagination-wrapper">
         <Button title="Previous" />

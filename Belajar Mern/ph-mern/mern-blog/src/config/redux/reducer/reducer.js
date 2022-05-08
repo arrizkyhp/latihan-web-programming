c0cb8b02eec4ac93984818d8a69765f804129341
arrizkyhp <52059831+arrizkyhp@users.noreply.github.com>
2022-05-08 09:46:50 +0700

    update Belajar Mern (ph-mern 50 | Memindahkan Posting Baru ke Redux)

--- Belajar Mern/ph-mern/mern-blog/src/config/redux/reducer/reducer.js
+++ Belajar Mern/ph-mern/mern-blog/src/config/redux/reducer/reducer.js
@@ -1,8 +1,9 @@
 import { combineReducers } from "redux"
 import globalReducer from "./globalReducer"
 import homeReducer from "./homeReducer"
+import createBlogReducer from "./createBlogReducer"
 
 // mengkombinasikan Reducer yang terpisah
-const reducer = combineReducers({ homeReducer, globalReducer })
+const reducer = combineReducers({ homeReducer, globalReducer, createBlogReducer })
 
 export default reducer;
--- Belajar Mern/ph-mern/mern-blog/src/pages/CreateBlog/index.jsx
+++ Belajar Mern/ph-mern/mern-blog/src/pages/CreateBlog/index.jsx
@@ -1,46 +1,31 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './createBlog.scss'
 import { Button, Input, TextArea, Upload } from '../../components'
-import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { postToAPI, setForm, setImgPreview } from '../../config/redux/action';
 
 const CreateBlog = () => {
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
-  const [image, setImage]= useState("");
-  const [imagePreview, setImagePreview] = useState(null)
+  const { form, imgPreview } = useSelector(state => state.createBlogReducer)
+  const { title, body} = form;
+  const dispatch = useDispatch()
 
   const handleSubmit = () => {
-    const data = new FormData();
-    data.append('title', title)
-    data.append('body', body)
-    data.append('image', image)
-
-    axios.post('http://localhost:4000/v1/blog/post', data, {
-      headers: {
-        'content-type': 'multipart/form-data'
-      }
-    })
-    .then(res => {
-      console.log('post success: ', res)
-    })
-    .catch(err => {
-      console.log('err: ', err)
-    })
+   postToAPI(form)
   }
 
   const onImageUpload = (e) => {
     const file = e.target.files[0]
-    setImage(file)
+    dispatch(setForm('image', file))
     // Generate Preview image
-    setImagePreview(URL.createObjectURL(file))
+    dispatch(setImgPreview(URL.createObjectURL(file)))
   }
 
   return (
     <div className='blog-post'>
       <p className='title'>Create New Blog Page</p>
-      <Input label="Post Title" value={title} onChange={(e) => setTitle(e.target.value)}/>
-      <Upload onChange={onImageUpload} img={imagePreview}/>
-      <TextArea value={body} onChange={(e) => setBody(e.target.value)}/>
+      <Input label="Post Title" value={title} onChange={(e) => dispatch(setForm('title', e.target.value))}/>
+      <Upload onChange={onImageUpload} img={imgPreview}/>
+      <TextArea value={body} onChange={(e) => dispatch(setForm('body', e.target.value))}/>
       <div className="button-action">
         <Button title="save" onClick={handleSubmit}/>
       </div>

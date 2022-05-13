a5198d16bade3e81d53278092703f2a7f02b0a9b
arrizkyhp <arrizkyhasya@gmail.com>
2022-05-13 15:32:58 +0700

    update Belajar React (brad-traversy Section 3: Components, Props & State)

---
+++ Belajar React/brad_traversy-react_ftb2022/src/components/FeedbackItem.jsx
@@ -0,0 +1,24 @@
+import Card from "./shared/Card"
+import PropTypes from 'prop-types'
+import { FaTimes } from 'react-icons/fa'
+
+const FeedbackItem = ({rating, text, id, handleDelete}) => {
+    
+  return (
+    <Card>
+        <div className="num-display">{rating}</div>
+        <button className="close" onClick={() => handleDelete(id)}>
+            <FaTimes color="purple" />
+        </button>
+        <div className="text-display">{text}</div>
+    </Card>
+  )
+}
+
+FeedbackItem.propTypes = {
+    id: PropTypes.number.isRequired,
+    rating: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired
+}
+
+export default FeedbackItem

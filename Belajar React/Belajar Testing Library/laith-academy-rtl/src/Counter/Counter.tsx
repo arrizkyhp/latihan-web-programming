ff8d3d2ac8b04a042a9f521ea717aee87718782a
arrizkyhp <arrizkyhasya@gmail.com>
2022-06-20 16:39:54 +0700

    update Belajar React Testing Library (laith-academy-rtl | fireEvent)

--- Belajar React/Belajar Testing Library/laith-academy-rtl/src/Counter/Counter.tsx
+++ Belajar React/Belajar Testing Library/laith-academy-rtl/src/Counter/Counter.tsx
@@ -2,21 +2,43 @@ import React, { useState } from 'react'
 import './Counter.css'
 
 const Counter = () => {
-  const [counterValue, setCounterValue] = useState(0);
-  const [inputValue, setInputValue] = useState(1)
+  const [counterValue, setCounterValue] = useState<number>(0);
+  const [inputValue, setInputValue] = useState<number>(1)
+
+  const subtractCounter = () => {
+    setCounterValue(counterValue - inputValue)
+  }
+
+  const addCounter = () => {
+    setCounterValue(counterValue + inputValue)
+  }
+
+
   return (
     <div>
         <h1 data-testid="header">My Counter</h1>
-        <h2 data-testid="counter">{counterValue}</h2>
+        <h2 
+          data-testid="counter" 
+          className={`${counterValue >= 100 ? "counter--green" : ""}${counterValue <= -100 ? "counter--red" : ""}`}
+        >
+          {counterValue}
+        </h2>
         <div className="counter-area">
-          <button data-testid="btn-subtract">-</button>
+          <button 
+            data-testid="btn-subtract"
+            onClick={subtractCounter}
+          >-</button>
           <input 
             className='counter-input' 
             data-testid="counter-input" 
             type="number" 
             value={inputValue} 
+            onChange={(e) => setInputValue(e.target.valueAsNumber)}
           />
-          <button data-testid="btn-add">+</button>
+          <button 
+            data-testid="btn-add"
+            onClick={addCounter}
+          >+</button>
         </div>
     </div>
   )
--- Belajar React/Belajar Testing Library/laith-academy-rtl/src/Counter/__test__/Counter.test.tsx
+++ Belajar React/Belajar Testing Library/laith-academy-rtl/src/Counter/__test__/Counter.test.tsx
@@ -1,5 +1,5 @@
 import Counter from "../Counter";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import '@testing-library/jest-dom/extend-expect'
 
 test("header renders with correct text", () => {
@@ -41,5 +41,159 @@ test("subtract button renders with -", () => {
     const subtractButton = screen.getByTestId("btn-subtract")
 
     expect(subtractButton.textContent).toBe("-")
+
+})
+
+test("change input value works correctly", () => {
+    render(<Counter />)
+
+    const inputEl = screen.getByTestId("counter-input") as HTMLInputElement
+
+    expect(inputEl.value).toBe("1")
+
+    fireEvent.change(inputEl, {
+        target: {
+            value: "5"
+        }
+    })
+
+    expect(inputEl.value).toBe("5")
+
+})
+
+test("click on + button add 1 to counter", () => {
+    render(<Counter />)
+
+    const addButton = screen.getByTestId("btn-add")
+    const counterEl = screen.getByTestId("counter")
+
+    expect(counterEl.textContent).toBe("0")
+
+    fireEvent.click(addButton)
+
+    expect(counterEl.textContent).toBe("1")
+})
+
+test("click on - button subtract 1 to counter", () => {
+    render(<Counter />)
+
+    const subtractButton = screen.getByTestId("btn-subtract")
+    const counterEl = screen.getByTestId("counter")
+
+    expect(counterEl.textContent).toBe("0")
+
+    fireEvent.click(subtractButton)
+
+    expect(counterEl.textContent).toBe("-1")
+})
+
+test("change input value then click add button works correctly", () => {
+    render(<Counter />)
+
+    const addButton = screen.getByTestId("btn-add")
+    const counterEl = screen.getByTestId("counter")
+    const inputEl = screen.getByTestId("counter-input") as HTMLInputElement
+
+    fireEvent.change(inputEl, {
+        target: {
+            value: "5"
+        }
+    })
+
+    fireEvent.click(addButton)
+
+    expect(counterEl.textContent).toBe("5")
+})
+
+test("change input value then click subtract button works correctly", () => {
+    render(<Counter />)
+
+    const subtractButton = screen.getByTestId("btn-subtract")
+    const counterEl = screen.getByTestId("counter")
+    const inputEl = screen.getByTestId("counter-input") as HTMLInputElement
+
+    fireEvent.change(inputEl, {
+        target: {
+            value: "5"
+        }
+    })
+
+    fireEvent.click(subtractButton)
+
+    expect(counterEl.textContent).toBe("-5")
+})
+
+test("adding and subtracting leads to correct counter number", () => {
+    render(<Counter />)
+
+    const addButton = screen.getByTestId("btn-add")
+    const counterEl = screen.getByTestId("counter")
+    const inputEl = screen.getByTestId("counter-input") as HTMLInputElement
+    const subtractButton = screen.getByTestId("btn-subtract")
+
+    fireEvent.change(inputEl, {
+        target: {
+            value: "10"
+        }
+    })
+
+    fireEvent.click(addButton)
+    fireEvent.click(addButton)
+    fireEvent.click(addButton)
+    fireEvent.click(addButton)
+    fireEvent.click(addButton)
+    fireEvent.click(subtractButton)
+    fireEvent.click(subtractButton)
+
+    expect(counterEl.textContent).toBe("30")
+
+    fireEvent.change(inputEl, {
+        target: {
+            value: "5"
+        }
+    })
+
+    fireEvent.click(addButton)
+    fireEvent.click(addButton)
+    fireEvent.click(addButton)
+    fireEvent.click(addButton)
+    fireEvent.click(subtractButton)
+
+    expect(counterEl.textContent).toBe("45")
+})
+
+test("counter contains correct className", () => {
+    render(<Counter />)
+
+    const counterEl = screen.getByTestId("counter")
+    const inputEl = screen.getByTestId("counter-input") as HTMLInputElement
+    const subtractButton = screen.getByTestId("btn-subtract")
+    const addButton = screen.getByTestId("btn-add")
+
+    expect(counterEl.className).toBe("")
+
+    fireEvent.change(inputEl, {
+        target: {
+            value: "50"
+        }
+    })
+
+    fireEvent.click(addButton)
+    
+    expect(counterEl.className).toBe("")
+    
+    fireEvent.click(addButton)
+    
+    expect(counterEl.className).toBe("counter--green")
+    
+    fireEvent.click(subtractButton)
+    fireEvent.click(subtractButton)
+    
+    expect(counterEl.className).toBe("")
+    
+    fireEvent.click(subtractButton)
+    fireEvent.click(subtractButton)
+
+    expect(counterEl.className).toBe("counter--red")
 
 })

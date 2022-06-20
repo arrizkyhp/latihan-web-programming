e3d1972c90922c13a35b6d7257ec90653e205021
arrizkyhp <arrizkyhasya@gmail.com>
2022-06-20 15:47:40 +0700

    update Belajar React Testing Library (laith-academy-rtl | inpur dom test)

--- Belajar React/Belajar Testing Library/laith-academy-rtl/src/Counter/Counter.tsx
+++ Belajar React/Belajar Testing Library/laith-academy-rtl/src/Counter/Counter.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Counter.css'
 
 const Counter = () => {
+  const [counterValue, setCounterValue] = useState(0);
+  const [inputValue, setInputValue] = useState(1)
   return (
     <div>
-        <h1 data-testid="header">Counter</h1>
+        <h1 data-testid="header">My Counter</h1>
+        <h2 data-testid="counter">{counterValue}</h2>
+        <div className="counter-area">
+          <button data-testid="btn-subtract">-</button>
+          <input 
+            className='counter-input' 
+            data-testid="counter-input" 
+            type="number" 
+            value={inputValue} 
+          />
+          <button data-testid="btn-add">+</button>
+        </div>
     </div>
   )
 }
--- Belajar React/Belajar Testing Library/laith-academy-rtl/src/Counter/__test__/Counter.test.tsx
+++ Belajar React/Belajar Testing Library/laith-academy-rtl/src/Counter/__test__/Counter.test.tsx
@@ -7,5 +7,39 @@ test("header renders with correct text", () => {
 
     const headerEl = screen.getByTestId("header")
 
-    expect(headerEl.textContent).toBe("Counter")
+    expect(headerEl.textContent).toBe("My Counter")
+})
+
+test("counter intially start at 0", () => {
+    render(<Counter />)
+
+    const counterEl = screen.getByTestId("counter")
+
+    expect(counterEl.textContent).toBe("0")
+})
+
+test("input contains intial value of 1", () => {
+    render(<Counter />)
+
+    const inputEl = screen.getByTestId("counter-input") as HTMLInputElement
+
+    expect(inputEl.value).toBe("1")
+})
+
+test("add button renders with +", () => {
+    render(<Counter />)
+
+    const addButton = screen.getByTestId("btn-add")
+
+    expect(addButton.textContent).toBe("+")
+
+})
+
+test("subtract button renders with -", () => {
+    render(<Counter />)
+
+    const subtractButton = screen.getByTestId("btn-subtract")
+
+    expect(subtractButton.textContent).toBe("-")
+
 })
